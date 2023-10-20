c456217ee36bb8d9c65d6f61885b8f6bd9366eee
gogwangsin <gwangsin1029@naver.com>
2023-10-21 01:27:03 +0900

    kingdom클래스, game_world로 코드 리펙토링

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 from pico2d import *
 
+import game_world
+from kingdom import KingDom
+
+
 def handle_event():
     global running
 
@@ -10,24 +14,54 @@ def handle_event():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        else:
+            pass
+            # knight.handle_event(event)
 
 
+def create_world():
+    global running
+    global kingdom
+    global knight
+    global enemy
 
-screen_width = 1280
-screen_height = 800
+    running = True
 
-open_canvas(screen_width, screen_height)
+    kingdom = KingDom(screen_width, screen_height)
+    game_world.add_object(kingdom, 0) # 제일 먼저
 
-background_image = load_image("BackGround\\road_background.png")
-background_image.draw(screen_width // 2 , screen_height // 2)
-update_canvas()
+    # knight = Knight()
+    # game_world.add_object(Knight, 1) # 두번째 레이어
 
-running = True
 
-show_lattice()
-while running:
+def open_screen():
+    global screen_width
+    global screen_height
+
+    screen_width = 1280
+    screen_height = 800
+
+    open_canvas(screen_width, screen_height)
+
+
+def update_world():
+    game_world.update()
+
+
+def render_world():
     clear_canvas()
-    handle_event()
-    background_image.clip_draw(0, 0, screen_width, screen_height, screen_width // 2, screen_height // 2 - 45, 1280, 720)
+    game_world.render()
     update_canvas()
+
+
+
+open_screen()
+create_world()
+# show_lattice()
+while running:
+    handle_event()
+    update_world()
+    render_world()
     delay(0.01)
+
+close_canvas()
