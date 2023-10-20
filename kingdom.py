f1fe39decd67359f00b6b9ec2fdfe212ed0227a8
gogwangsin <gwangsin1029@naver.com>
2023-10-21 01:26:24 +0900

    clip_draw에 사용할 값을 변수화 (사진 원본 크기, 그릴 크기, 화면 스크린 크기, 그릴 위치 )

--- kingdom.py
+++ kingdom.py
@@ -8,21 +8,23 @@ class KingDom:
     def __init__(self, screen_width, screen_height):
         self.image = load_image("BackGround\\road_background.png")
 
+        self.image_size_width = 1280
+        self.image_size_height = 574 # 사진 원본 크기
+
+        self.draw_size_width = 1280
+        self.draw_size_height = 720 # 사진 그릴 크기
+
         self.screen_width = screen_width
-        self.screen_height = screen_height # 화면 스크린 좌표 저장
+        self.screen_height = screen_height # 화면 스크린 크기
 
         self.draw_x = self.screen_width // 2 # 사진 그릴 위치 (x,y)
-        self.draw_y = self.screen_height // 2 - 45
+        self.draw_y = self.screen_height // 2 - 45 # 사진 비율 조정으로 인한 y 조절
 
     def draw(self):
-        # self.image.draw(self.x, self.y)
-        self.image.clip_draw(0, 0, self.screen_width, self.screen_height, self.draw_x, self.draw_y, 1280, 720)
-        # self.image.clip_draw(0, 0, self.screen_width, self.screen_height, self.screen_width // 2, self.screen_height // 2 - 45, 1280, 720)
-
-        # 사진 그릴 중심 (왼쪽 하단 0,0) / 사진 크기 / 그릴위치(사진중앙이 pivot) // 그릴 크기 조정 1280, 720
-
-        # 사진 왼쪽아래(x), 사진(y), 그릴크기(x), 그릴(y), 실제x, y
-        # clip_draw(frame * 100, 0, 100, 100, x, 130, 200, 200)
+        self.image.clip_draw(0, 0, self.image_size_width, self.image_size_height,
+                             self.draw_x, self.draw_y, self.draw_size_width, self.draw_size_height)
+        # 사진 픽셀 시작 위치 (왼쪽 하단 0,0) / 사진 크기 / 그릴 위치(사진 중앙이 pivot) // 그릴 크기 조정 1280, 720
+        # (0,0, 1280, 574, drawX,drawY, 1280, 720)
 
     def update(self):
         pass
