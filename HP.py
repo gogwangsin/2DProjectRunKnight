289a70ad6d37579fc7a0a2245357b8db58fc92ff
gogwangsin <gwangsin1029@naver.com>
2023-10-21 21:51:59 +0900

    sacle_factor로 hp이미지의 그릴 가로 길이의 비율을 조정하고 draw_x 값을 변경 시켜서 체력바가 오른쪽만 줄어드는 것 처럼 보이게 함

--- HP.py
+++ HP.py
@@ -6,24 +6,25 @@ class HP:
         self.knight = knight
 
         self.hp_image = load_image("UI\\HP_bar.png")
-        self.hp_bar_image = load_image("UI\\HP_back_bar.png")
+        self.hp_template_image = load_image("UI\\HP_back_bar.png")
 
         self.image_size_width = 657
         self.image_size_height = 37  # 한개 사진 크기
 
-        self.draw_size_width = 657 * 1.0  #
+        self.scale_factor = 0.7
+        self.draw_size_width = 657 * self.scale_factor  #
         self.draw_size_height = 37 * 1.0  # 사진 그릴 크기 [ 비율 조정 ]
 
         self.draw_x, self.draw_y = 640, 750 # 그릴 피봇 -> 스크린 크기 ( 1280, 800 )
 
     def update(self):
+        self.draw_x = 640 - (self.image_size_width - self.draw_size_width) / 2
         print('용사 체력 :', self.knight.get_hp()) # hp에 따라 길이 조절
         pass
 
     def draw(self):
-        self.hp_bar_image.clip_draw(0, 0, 675, 84,
-                                self.draw_x, self.draw_y, 675, 37*1.5)
+        # HP틀 크기 675, 84 [ 상수 -> 안변함 ]
+        self.hp_template_image.clip_draw(0, 0, 675, 84, 640, 750, 675, 37 * 1.5)
 
         self.hp_image.clip_draw(0, 0, self.image_size_width, self.image_size_height,
                                 self.draw_x, self.draw_y, self.draw_size_width, self.draw_size_height)
-
