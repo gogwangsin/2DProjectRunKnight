57523f4e82a72fc70ac28fa4e221b1d5996f9539
gogwangsin <gwangsin1029@naver.com>
2023-10-26 01:07:54 +0900

    knight객체의 상태머신 StateMachine클래스 생성과  Idle 상태 print 확인

--- knight.py
+++ knight.py
@@ -3,11 +3,49 @@ import global_var
 
 # 용사 객체
 
+class Idle:
+    @staticmethod # 함수를 그룹핑 하는 역할
+    def do():
+        print('Idle Doing')
+        pass
+
+    @staticmethod
+    def entry():
+        print('Idle Entry Action')
+        pass
+
+    @staticmethod
+    def exit():
+        print('Idle Exit Action')
+        pass
+
+#==========================================================
+class StateMachine:
+    def __init__(self):
+        self.cur_state = Idle
+        pass
+
+    def start(self):
+        self.cur_state.entry() # entry action
+        pass
+
+    def update(self):
+        self.cur_state.do()
+        pass
+
+    def draw(self):
+        pass
+
+
+#==========================================================
 class Knight:
     def __init__(self):
         self.init_knight_var()
+        self.init_state_machine()
 
     def update(self):
+
+        self.state_machine.update()
 
         if self.get_time_gap() > self.update_frame_time:
             self.knight_frame = (self.knight_frame + 1) % 3
@@ -16,27 +54,19 @@ class Knight:
         self.knight_HP -= 0.25
         if self.knight_HP <= 0:
             print('용사 사망')
-            global_var.running = False
+            # global_var.running = False
+            self.knight_HP = 100
             return
 
         # global_var.scroll_speed = 0
-
-
-    def get_time_gap(self):
-        return get_time() - self.last_frame_update_time
-
-    def handle_event(self, event):
-        pass
 
     def draw(self):
         self.knight_image.clip_draw(self.knight_frame * self.knight_width, self.knight_action * self.knight_height,
                                     self.knight_width, self.knight_height, self.knight_draw_x, self.knight_draw_y,
                                     self.knight_draw_width, self.knight_draw_height)
 
-
-    def get_current_HP(self):
-        return self.knight_HP
-
+    def handle_event(self, event):
+        pass
 
     def init_knight_var(self):
         self.knight_image = load_image("Object\\KnightSprite.png")
@@ -54,4 +84,17 @@ class Knight:
         self.knight_HP = 100
 
         self.last_frame_update_time = get_time()
-        self.update_frame_time = 0.08  # 프레임 업데이트 시간 간격 : 0.8 -> 스크롤 속도에 영향 받음
+        self.update_frame_time = 0.065  # 프레임 업데이트 시간 간격 : 0.8 -> 스크롤 속도에 영향 받음 : 0.08
+
+
+    def init_state_machine(self):
+        self.state_machine = StateMachine()
+        self.state_machine.start()
+
+    def get_time_gap(self):
+        return get_time() - self.last_frame_update_time
+
+    def get_current_HP(self):
+        return self.knight_HP
+
+
