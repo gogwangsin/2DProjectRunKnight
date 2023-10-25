1658c07c09d4fd217940b9e770d6fc8ad9003da5
gogwangsin <gwangsin1029@naver.com>
2023-10-25 19:17:13 +0900

    running global_var 모듈로 옮김

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 from pico2d import *
 
 import game_world
+import global_var
+
 from GUI import GUI
 from kingdom import KingDom
 from knight import Knight
 
 
 def handle_event():
-    global running
     events = get_events()
 
     for event in events:
         if event.type == SDL_QUIT:  # 나가기 클릭
-            running = False
+            global_var.running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:  # ESC키 나가기
-            running = False
+            global_var.running = False
         else:
             knight.handle_event(event)
 
 
 def create_world():
-    global running
     global kingdom
     global knight
     global enemy
     global gui
 
-    running = True
+    global_var.init_global_var()
 
-    # 레이어 규칙 : 나중 레이어가 덮는다 ( 0 < 1 )
-    kingdom = KingDom(screen_width, screen_height)
-    game_world.add_object(kingdom, 0)  # 제일 먼저
+    kingdom = KingDom(screen_width, screen_height, global_var.scroll_speed)
+    game_world.add_object(kingdom, 0)
 
     knight = Knight()
-    game_world.add_object(knight, 1)  # 두번째 레이어
+    game_world.add_object(knight, 1)
 
     gui = GUI(knight)
     game_world.add_object(gui, 2)
@@ -45,7 +44,6 @@ def open_screen():
 
     screen_width = 1280
     screen_height = 800
-
     open_canvas(screen_width, screen_height)
 
 
@@ -61,7 +59,7 @@ def render_world():
 
 open_screen()
 create_world()
-while running:
+while global_var.running:
     handle_event()
     update_world()
     render_world()
