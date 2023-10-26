eb986328fc7036000e0504e65037264fb6071f81
gogwangsin <gwangsin1029@naver.com>
2023-10-26 20:36:23 +0900

    상태 변환 테이블인 transitions 딕셔너리에 상태(Walk)를 key값으로 value에 딕셔너리는 넣고 이 딕셔너리엔 key에 event, value에 상태를 넣어 동적으로 변하게 했으며 키 이벤트를 통해 용사가 위아래로 움직일 수 있도록 함

--- knight.py
+++ knight.py
@@ -1,29 +1,55 @@
 from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDLK_LEFT, SDL_KEYUP, SDLK_RIGHT
+
 import global_var
 
 
 # 용사 객체
 
+def left_down(event):
+    return event[0] == 'INPUT' and event[1].type == SDL_KEYDOWN and event[1].key == SDLK_LEFT
+
+
+def left_up(event):
+    return event[0] == 'INPUT' and event[1].type == SDL_KEYUP and event[1].key == SDLK_LEFT
+
+
+def right_down(event):
+    return event[0] == 'INPUT' and event[1].type == SDL_KEYDOWN and event[1].key == SDLK_RIGHT
+
+
+def right_up(event):
+    return event[0] == 'INPUT' and event[1].type == SDL_KEYUP and event[1].key == SDLK_RIGHT
+
+
 class Walk:
     @staticmethod
     def entry(knight, event):
-        print('Idle Entry Action')
-        knight.frame = 0
-        knight.action = 0  # 0 걷기, 1 찌르기 2, 점프 공격
+        print('Walk Entry Action')
+        # 키 flag : 한번 눌렀을 때 방향 정해짐
+        if left_down(event):
+            knight.Dir += 1
+        elif right_down(event):
+            knight.Dir -= 1
+        elif left_up(event):
+            knight.Dir -= 1
+        elif right_up(event):
+            knight.Dir += 1
 
+        knight.action = 0  # 0 걷기, 1 찌르기 2, 점프 공격
         knight.last_frame_time = get_time()
-        knight.update_frame_time = 0.065  # 프레임 업데이트 시간 간격 : 0.8 -> 스크롤 속도에 영향 받음 : 0.08
+        knight.update_frame_time = 0.085  # 프레임 업데이트 시간 간격 : 0.8 -> 스크롤 속도에 영향 받음 : 0.08
 
     @staticmethod  # 함수를 그룹핑 하는 역할
     def do(knight):
-        print('Idle Doing')
         if get_time() - knight.last_frame_time > knight.update_frame_time:
             knight.frame = (knight.frame + 1) % 3
             knight.last_frame_time = get_time()
+        knight.knight_draw_y += knight.Dir * 6.5
 
     @staticmethod
     def exit(knight, event):
-        print('Idle Exit Action')
+        print('Walk Exit Action')
         pass
 
     @staticmethod
@@ -40,11 +66,17 @@ class StateMachine:
     def __init__(self, knight):
         self.knight = knight
         self.cur_state = Walk
+        # 상태 전환 테이블
+        self.transitions = {
+
+            Walk: {left_down: Walk, left_up: Walk, right_down: Walk, right_up: Walk}
+
+        }
         pass
 
     def start(self):
         self.cur_state.entry(self.knight, ('START', 0))
-        # entry action : key == START, event값 0으로 전달
+        # entry action : event:( key == START, value == 0 )으로 전달
 
     def update(self):
         self.cur_state.do(self.knight)
@@ -54,8 +86,15 @@ class StateMachine:
         self.cur_state.draw(self.knight)
         pass
 
-    def handle_event(self):
-        pass
+    def handle_event(self, event):
+        # cur_state의 key,value값 하나씩 꺼내서 확인 [ex) left_down: Walk, left_up: Walk]
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(event):
+                self.cur_state.exit(self.knight, event)
+                self.cur_state = next_state
+                self.cur_state.entry(self.knight, event)
+                return True
+        return False
 
 
 # ==========================================================
@@ -66,21 +105,15 @@ class Knight:
 
     def update(self):
         self.state_machine.update()
+        self.update_HP()
 
-        self.knight_HP -= 0.25
-        if self.knight_HP <= 0:
-            print('용사 사망')
-            # global_var.running = False
-            self.knight_HP = 100
-            return
-
-        # global_var.scroll_speed = 0
+        # global_var.scroll_speed 대쉬에 따라 속도 변화
 
     def draw(self):
         self.state_machine.draw()
 
     def handle_event(self, event):
-        self.state_machine.handle_event()
+        self.state_machine.handle_event(('INPUT', event))  # 입력 이벤트
 
     def init_knight_var(self):
         self.knight_image = load_image("Object\\KnightSprite.png")
@@ -93,11 +126,22 @@ class Knight:
 
         self.knight_draw_x, self.knight_draw_y = 250, 400
 
-        self.knight_HP = 100
+        self.frame = 0
+        self.HP = 100
+        self.Dir = 0
 
     def init_state_machine(self):
         self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def get_current_HP(self):
-        return self.knight_HP
+        return self.HP
+
+    def update_HP(self):
+        self.HP -= 0.25
+        if self.HP <= 0:
+            print('용사 사망')
+            # global_var.running = False
+            # global_var.scroll_speed = 0
+            self.HP = 100
+            return
