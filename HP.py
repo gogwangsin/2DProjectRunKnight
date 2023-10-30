c4a415af625bb244ccb7c8bb3dac401875c1a956
gogwangsin <gwangsin1029@naver.com>
2023-10-30 12:44:00 +0900

    hp클래스 초기화 리펙토링

--- HP.py
+++ HP.py
@@ -1,49 +1,36 @@
 from pico2d import load_image
 
+
 # HP 클래스
 
 
 class HP:
     def __init__(self, knight):
         self.knight = knight
-
-        self.hp_image = load_image("UI\\HP_bar.png")
-        self.hp_template_image = load_image("UI\\HP_back_bar.png")
-
-        self.image_size_width = 657
-        self.image_size_height = 37  # 한개 사진 크기
-
-        self.scale_factor = 1.0
-        self.draw_size_width = 657 * 1.0  #
-        self.draw_size_height = 37 * 1.0  # 사진 그릴 크기 [ 비율 조정 ]
-
-        self.draw_x, self.draw_y = 640, 750 # 그릴 피봇 -> 스크린 크기 ( 1280, 800 )
+        self.init_hp_var()
 
     def update(self):
-        self.scale_factor = self.knight.get_hp() / 100 # 현재체력 / 최대체력 -> 비율
-        self.draw_size_width = 657 * self.scale_factor  # 그릴 길이 x 체력비율 매핑
-
-        self.draw_x = 640 - (self.image_size_width - self.draw_size_width) / 2
-        print('용사 체력 :', self.knight.get_hp()) # hp에 따라 길이 조절 -> scale_factor 수정\
-
-        if( self.knight.get_hp() == 0):
-            print('HP 0')
-            running = False
-        pass
+        self.scale_factor = self.knight.get_current_hp() / 100  # 현재체력 / 최대체력 -> 비율
+        self.HP_draw_width = 657 * self.scale_factor  # 그릴 길이 x 체력비율 매핑
+        self.HP_draw_x = 640 - (self.HP_width - self.HP_draw_width) / 2
+        # print('용사 체력 :', self.knight.get_current_HP())  # hp에 따라 길이 조절 -> scale_factor 수정\
 
     def draw(self):
-        # HP틀 크기 675, 84 [ 상수 -> 안변함 ]
-        self.hp_template_image.clip_draw(0, 0, 675, 84, 640, 750, 675, 37 * 1.5)
+        # HP 템플릿 크기 675, 84 [ 상수 -> 안변함 ]
+        self.HP_template_image.clip_draw(0, 0, 675, 84, 640, 750, 675, 37 * 1.5)
 
-        self.hp_image.clip_draw(0, 0, self.image_size_width, self.image_size_height,
-                                self.draw_x, self.draw_y, self.draw_size_width, self.draw_size_height)
+        self.HP_image.clip_draw(0, 0, self.HP_width, self.HP_height,
+                                self.HP_draw_x, self.HP_draw_y, self.HP_draw_width, self.HP_draw_height)
 
+    def init_hp_var(self):
+        self.HP_image = load_image("UI\\HP_bar.png")
+        self.HP_template_image = load_image("UI\\HP_back_bar.png")
 
+        self.HP_width = 657
+        self.HP_height = 37  # 한개 사진 크기
 
-# 원래 self.draw_x는 사진을 그릴 위치를 가리키는데,
-# 원래 크기와 새로운 크기 간의 차이를 반으로 나누어 오른쪽으로 이동한다.
-# -> 줄어든 사진 크기의 절반 값을 원래의 draw_x에 +-한다.
+        self.scale_factor = 1.0
+        self.HP_draw_width = 657 * 1.0  #
+        self.HP_draw_height = 37 * 1.0  # 사진 그릴 크기 [ 비율 조정 ]
 
-# 결과적으로, 비율을 조정하고 오른쪽으로 이동시켜 가운데 정렬을 유지함으로써
-# 원하는 비율로 축소된 사진을 그릴 수 있다.
-# 이러한 방식으로 사진을 조정하면 화면 상에서 오른쪽으로만 줄어들게 된다.
+        self.HP_draw_x, self.HP_draw_y = 640, 750  # 그릴 피봇 -> 스크린 크기 ( 1280, 800 )
