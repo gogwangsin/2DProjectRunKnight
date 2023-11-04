2cafc093de334781cb8f253d1fa10f753bb70753
gogwangsin <gwangsin1029@naver.com>
2023-11-04 23:02:52 +0900

    게임 프레임 워크 모듈로 main파일 생성

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import pico2d
-import global_var
-import play_mode
+from pico2d import close_canvas
+
+import game_framework
+import play_mode as start_mode
+# import logo_mode as start_mode
 
 
-play_mode.open_screen()
-play_mode.init()
-while global_var.running:
-    play_mode.handle_event()
-    play_mode.update_world()
-    play_mode.draw()
-    pico2d.delay(0.01)
-play_mode.finish()
-pico2d.close_canvas()
+game_framework.run(start_mode)
+close_canvas()
