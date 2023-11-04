ac0782713fc67be031a398ad9a69f0b76be4624b
gogwangsin <gwangsin1029@naver.com>
2023-11-04 23:01:51 +0900

    모든 모듈 전역변수 scroll_speed 선언

--- global_var.py
+++ global_var.py
@@ -1,12 +1,13 @@
 
 # 클래스 간 실시간으로 상호작용 하기 위한 모듈 from이 아닌 import global_variable로 해야 적용된다.
 
-def init_global_var():
-    global running
-    global scroll_speed
+scroll_speed = None
 
-    running = True
-    scroll_speed = 6.4
-    # scroll_speed = 0 # 6.4
+# def init_global_var():
+#     # global running
+#     global scroll_speed
+#     # running = True
+#     scroll_speed = 6.4
+#     # scroll_speed = 0 # 6.4
 
 
