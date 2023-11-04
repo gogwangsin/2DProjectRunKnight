0b7d5ac097642c2870fa91f612ffc07dad63890c
gogwangsin <gwangsin1029@naver.com>
2023-11-05 04:21:40 +0900

    중앙 결과창 위치 저장

--- result_pannel.py
+++ result_pannel.py
@@ -1,11 +1,15 @@
 from pico2d import load_image
 
+# screen_width = 1280, height = 800
 class ResultPannel:
     def __init__(self):
         self.menu = load_image("UI\\gold_meter_bar.png")
+        self.screen_width = 1280
+        self.screen_height = 800
 
     def draw(self):
-        self.menu.draw(400, 300)
+        # self.menu.draw(400, 300)
+        self.menu.clip_draw(0, 0, 667, 426, self.screen_width // 2, self.screen_height // 2, 667, 426)
 
     def update(self):
         pass
