80deb5a372c256f16982a0b4f2d2bc65ba2cfc79
gogwangsin <gwangsin1029@naver.com>
2023-11-04 23:17:17 +0900

    게임모드 이미지 추가

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pico2d import close_canvas
 
 import game_framework
-import play_mode as start_mode
-# import logo_mode as start_mode
+# import play_mode as start_mode
+import logo_mode as start_mode
 
 
 game_framework.run(start_mode)
