14514be9a1bf36cc5d34316330bdebde81de442d
gogwangsin <gwangsin1029@naver.com>
2023-11-17 05:09:04 +0900

    대쉬gui 추가

--- GUI.py
+++ GUI.py
@@ -1,17 +1,20 @@
 # GUI 클래스
-from HP import HP
+from DashSkillGUI import DashGUI
+from HpGUI import HpGUI
 
 
 class GUI:
-
     def __init__(self, knight):
-        self.HP = HP(knight)
+        self.HP = HpGUI(knight)
+        self.DashSkill = DashGUI(knight)
         pass
 
     def update(self):
         self.HP.update()
+        self.DashSkill.update()
         pass
 
     def draw(self):
         self.HP.draw()
+        self.DashSkill.draw()
         pass
