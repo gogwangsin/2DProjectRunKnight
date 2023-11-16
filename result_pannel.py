106ca845fa2534e311adcd3e8e18dd656c23a074
gogwangsin <gwangsin1029@naver.com>
2023-11-16 13:33:07 +0900

    결과 패널 출력

--- result_pannel.py
+++ result_pannel.py
@@ -3,13 +3,17 @@ from pico2d import load_image
 # screen_width = 1280, height = 800
 class ResultPannel:
     def __init__(self):
-        self.menu = load_image("UI\\gold_meter_bar.png")
+        self.menu = load_image("UI\\gold_meter_bar.png")   # self.menu.draw(400, 300)
+        self.result_bar = load_image('UI\\result_bar.png') # result_bar 431 x 165
+        self.regame_button = load_image('UI\\regame_button.png') # regame_button 243 x 87
+        self.coin = load_image('UI\\coin.png') # coin 81 x 81
         self.screen_width = 1280
         self.screen_height = 800
 
     def draw(self):
-        # self.menu.draw(400, 300)
         self.menu.clip_draw(0, 0, 667, 426, self.screen_width // 2, self.screen_height // 2, 667, 426)
+        self.result_bar.clip_draw(0, 0, 431, 165, self.screen_width // 2, self.screen_height - 73, 431, 165)
+        self.regame_button.clip_draw(0, 0, 243, 87, self.screen_width // 2, 135, 243 * 1.1, 87 * 1.1)
 
     def update(self):
         pass
