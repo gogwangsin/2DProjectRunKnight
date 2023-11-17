2ef3427a9830d73d74cc9c2be5c6340dae48df0d
gogwangsin <gwangsin1029@naver.com>
2023-11-17 17:41:43 +0900

    코인, 이동거리 패널 추가

--- DistancePannelGUI.py
+++ DistancePannelGUI.py
@@ -13,15 +13,17 @@ class DistanceGUI:
         self.pannel_image = load_image("UI\\menu_meter.png")
         self.meter_image = load_image('UI\\meter.png')
         self.font = load_font('Jalnan.ttf', 25)
+        self.dis = 0
 
     def update(self):
+        self.dis += play_mode.scroll_pixel_per_second * game_framework.frame_time
         pass
     def draw(self):
         self.pannel_image.clip_draw(0, 0, 323, 138, 1150, 745, 323, 138)
         self.meter_image.clip_draw(0, 0, 500, 300, 1120, 732, 500 * 0.45, 300 * 0.45)
-        self.font.draw(1035, 700, '{:>5d}'.format(int(self.knight.Coin)), (126, 122, 113))
-        # self.font.draw(1035, 700, '{:>5d}'.format(int(play_mode.scroll_pixel_per_second * game_framework.frame_time)),
-        #                (126, 122, 113))
+        # self.font.draw(1035, 700, '{:>5d}'.format(int(self.knight.Coin)), (126, 122, 113))
+        self.font.draw(1035, 700, '{:>5d}'.format(int(self.dis)),
+                       (126, 122, 113))
 
         pass
 
--- GUI.py
+++ GUI.py
@@ -1,24 +1,32 @@
 # GUI 클래스
 from AngelSkillGUI import AngelGUI
 from DashSkillGUI import DashGUI
+from DistancePannelGUI import DistanceGUI
 from HpGUI import HpGUI
+from CoinPannelGUI import CoinGUI
 
 
 class GUI:
     def __init__(self, knight):
         self.HP = HpGUI(knight)
+        self.CoinPannel = CoinGUI(knight)
+        self.DisPannel = DistanceGUI(knight)
         self.DashSkill = DashGUI(knight)
         self.AngelSkill = AngelGUI(knight)
         pass
 
     def update(self):
         self.HP.update()
+        self.CoinPannel.update()
+        self.DisPannel.update()
         self.DashSkill.update()
         self.AngelSkill.update()
         pass
 
     def draw(self):
         self.HP.draw()
+        self.CoinPannel.draw()
+        self.DisPannel.draw()
         self.DashSkill.draw()
         self.AngelSkill.draw()
         pass
