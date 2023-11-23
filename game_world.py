bce4ea2bb38758917449f382c31262cb25cbacbb
gogwangsin <gwangsin1029@naver.com>
2023-11-23 20:48:44 +0900

    충돌 딕셔너리 리스트로 관리

--- game_world.py
+++ game_world.py
@@ -3,11 +3,13 @@
 # 두 개의 layer를 갖는 게임 월드로 구현
 
 objects = [[], [], []]
-
-
 # [0] : 배경
 # [1] : 객체-> Y정렬 가변 랜더링
 # [2] : UI
+collision_pairs = {}
+
+
+# 충돌 관점의 월드 정의 dictionary
 
 def add_object(_obj, depth=0):
     objects[depth].append(_obj)
@@ -29,6 +31,8 @@ def remove_object(_obj):
     for Layer in objects:  # 레이어에 안들어있는데 삭제하려면 오류남 [존재하지 않는 객체 삭제]
         if _obj in Layer:  # --> 만약 레이어 안에 있다면 지우기
             Layer.remove(_obj)  # 오브젝트 안에 있는 모든 레이어에 대해 지워라
+            remove_collision_object(_obj)
+            del _obj
             return  # 한 번 지웠으면 굳이 for 루프 돌 필요없다. -> 최적화
 
     raise ValueError('[오류] 없는데 왜 지우려고 하니?')  # 존재하지 않는 걸 지우려고 할 때
@@ -70,3 +74,30 @@ def collide(A, B):
     if BottomA > TopB: return False
 
     return True
+
+
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        print(f'New Group {group} added.')
+        collision_pairs[group] = [[], []]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def remove_collision_object(_obj):
+    for pairs in collision_pairs.values():
+        if _obj in pairs[0]:
+            pairs[0].remove(_obj)
+        if _obj in pairs[1]:
+            pairs[1].remove(_obj)
+
+
+def handle_collisions():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
