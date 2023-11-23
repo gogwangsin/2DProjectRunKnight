e4754df87796d40a2c7f9abe0e601f3396f36ed3
gogwangsin <gwangsin1029@naver.com>
2023-11-23 21:58:51 +0900

    바운딩 박스 리스트화

--- Coin.py
+++ Coin.py
@@ -6,6 +6,7 @@ import game_framework
 import game_world
 import play_mode
 
+
 def coin_add():
     if game_framework.current_time - play_mode.coin_start_time >= random.uniform(1.0, 5.0):
         coin = Coin()
@@ -16,6 +17,7 @@ def coin_add():
 
 class Coin:
     image = None
+
     def __init__(self):
         if self.image == None:
             self.image = load_image("Object\\coin_object.png")
@@ -25,26 +27,32 @@ class Coin:
         self.time_per_action = 0.4  # 하나의 액션이 소요되는 시간
         self.action_per_time = 1.0 / self.time_per_action  # 시간당 수행할 수 있는 액션 개수
         self.frames_per_action = 5  # 액션 당 필요한 프레임 수
-
-
+        self.bounding_box_list = []
 
     def update(self):
         if self.draw_x < -81:
             game_world.remove_object(self)
         self.frame = (self.frame + self.frames_per_action * self.action_per_time * game_framework.frame_time) % 5
         self.draw_x -= play_mode.scroll_pixel_per_second * game_framework.frame_time
-
+        self.update_bounding_box()
 
     def draw(self):
         self.image.clip_draw(int(self.frame) * 81, 0, 81, 81, self.draw_x, self.draw_y, 81, 81)
-        if play_mode.bb_toggle: draw_rectangle(*self.get_bounding_box())
+        if play_mode.bb_toggle:
+            for box in self.bounding_box_list:
+                draw_rectangle(*box)
 
     def handle_event(self, event):
         pass
 
     def get_bounding_box(self):
-        return self.draw_x - 30.0, self.draw_y - 37.5, self.draw_x + 30.0, self.draw_y + 37.5
+        return self.bounding_box_list
 
     def handle_collision(self, group, other):
         if group == 'Knight:Coin':
             game_world.remove_object(self)
+
+    def update_bounding_box(self):
+        self.bounding_box_list = [
+            (self.draw_x - 30.0, self.draw_y - 37.5, self.draw_x + 30.0, self.draw_y + 37.5)
+        ]
--- HpPortion.py
+++ HpPortion.py
@@ -20,26 +20,32 @@ class HPportion:
             self.image = load_image("Object\\portion_object.png")
         self.draw_x, self.draw_y = 1280 + 75, random.randint(50, 630) # 100
         self.layer_y = self.draw_y - 40.0
-        pass
+        self.bounding_box_list = []
 
     def update(self):
         if self.draw_x < -75:
             game_world.remove_object(self)
         self.draw_x -= play_mode.scroll_pixel_per_second * game_framework.frame_time
-
+        self.update_bounding_box()
 
 
     def draw(self):
         self.image.clip_draw(0, 0, 75, 91, self.draw_x, self.draw_y, 75, 91)
-        if play_mode.bb_toggle: draw_rectangle(*self.get_bounding_box())
-        pass
+        if play_mode.bb_toggle:
+            for box in self.bounding_box_list:
+                draw_rectangle(*box)
 
     def handle_event(self, event):
         pass
 
     def get_bounding_box(self):
-        return self.draw_x - 37.5, self.draw_y - 40.0, self.draw_x + 37.5, self.draw_y + 40
+        return self.bounding_box_list
 
     def handle_collision(self, group, other):
         if group == 'Knight:Portion':
             game_world.remove_object(self)
+
+    def update_bounding_box(self):
+        self.bounding_box_list = [
+            (self.draw_x - 37.5, self.draw_y - 40.0, self.draw_x + 37.5, self.draw_y + 40)
+        ]
