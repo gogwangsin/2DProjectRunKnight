f34610ec2b418a479bae3d464d0e1f8f6f497d91
gogwangsin <gwangsin1029@naver.com>
2023-11-27 21:27:24 +0900

    몬스터 어택 이펙트 2 추가

---
+++ MonsterAttackedEffect2.py
@@ -0,0 +1,30 @@
+from pico2d import load_image
+import game_framework
+import game_world
+import play_mode
+
+
+class MonsterAttacked2:
+    image = None
+
+    def __init__(self, monster):
+        if self.image == None:
+            self.image = load_image("Skill\\attack_sprite.png")
+        self.mon = monster
+        self.draw_x, self.draw_y = self.mon.draw_x, self.mon.draw_y
+        self.layer_y = self.draw_y - (203 / 2)
+        self.frame = 1
+        self.time_per_action = 0.4  # 하나의 액션이 소요되는 시간
+        self.action_per_time = 1.0 / self.time_per_action  # 시간당 수행할 수 있는 액션 개수
+        self.frames_per_action = 7  # 액션 당 필요한 프레임 수
+
+    def update(self):
+        self.frame = (self.frame + self.frames_per_action * self.action_per_time * game_framework.frame_time) % 7
+        if int(self.frame) == 6: self.remove()
+
+    def draw(self):
+        self.image.clip_draw(int(self.frame) * 374, 0, 374, 203, self.draw_x, self.draw_y, 374 * 0.95, 203 * 0.95)
+
+    def remove(self):
+        game_world.remove_object(self)
+        pass
