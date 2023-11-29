90e36514de4d70cb560a37753817ce90af889db6
gogwangsin <gwangsin1029@naver.com>
2023-11-29 19:33:08 +0900

    over_mode시 마우스 추가

--- over_mode.py
+++ over_mode.py
@@ -1,16 +1,16 @@
-from pico2d import clear_canvas, update_canvas, get_events, show_cursor
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_r
-
+from pico2d import clear_canvas, update_canvas, get_events, load_image
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_r, SDL_MOUSEMOTION
 import game_framework
 import game_world
 import play_mode
 import title_mode
 from ResultPannel import ResultPannel
 
+mouse_x, mouse_y = 0, 0
 
 def init():
-    global result
-    show_cursor()
+    global result, cursor_image
+    cursor_image = load_image('UI\\cursor.png')
     result = ResultPannel()
     game_world.add_object(result, 2)
 
@@ -26,10 +26,12 @@ def update():
 def draw():
     clear_canvas()
     game_world.render()
+    cursor_image.clip_draw(0, 0, 97, 92, mouse_x, mouse_y, 97 * 0.9, 92 * 0.9)
     update_canvas()
 
 
 def handle_events():
+    global mouse_x, mouse_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -40,6 +42,8 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_r:
             game_framework.pop_mode()
             game_framework.run(play_mode)
+        elif event.type == SDL_MOUSEMOTION:
+            mouse_x, mouse_y = event.x, 800 - 1 - event.y
 
 
 def pause():
